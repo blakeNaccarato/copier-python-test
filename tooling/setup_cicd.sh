6c48a062e6e5fb5bba56035348378115de5db4d7
blakeNaccarato <20692450+blakeNaccarato@users.noreply.github.com>
2021-09-19 13:46:53 -0700

    Bump tooling (blakeNaccarato/copier-python#16)

--- tooling/setup_cicd.sh
+++ tooling/setup_cicd.sh
@@ -1,6 +1,6 @@
 pip install -U pip wheel
 pip install pipx
 pipx install copier==6.0.0a7
-copier -f -r 8624aa4
-pip install -r tooling/requirements_cicd.txt
+copier -f -r 5ee0f72
+pip install -r requirements_cicd.txt
 flit install
