f780407a0bafab881ee33b967d015f00e28a3753
blakeNaccarato <20692450+blakeNaccarato@users.noreply.github.com>
2021-09-19 19:35:24 -0700

    Bump tooling (blakeNaccarato/copier-python#23)

--- tooling/setup.ps1
+++ tooling/setup.ps1
@@ -1,4 +1,4 @@
-copier -r 369ec2c
+copier -r d667e39
 py -3.9 -m venv .venv
 .venv/Scripts/activate
 pip install -U pip  # throws [WinError 5], but still works on its own
--- tooling/setup_cicd.sh
+++ tooling/setup_cicd.sh
@@ -1,6 +1,6 @@
 pip install -U pip wheel
 pip install pipx
 pipx install copier==6.0.0a7
-copier -f -r 369ec2c
+copier -f -r d667e39
 pip install -r tooling/requirements_cicd.txt
 flit install
