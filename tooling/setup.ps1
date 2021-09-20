c438aac6232839dc2370c61d0785cfe95296573e
blakeNaccarato <20692450+blakeNaccarato@users.noreply.github.com>
2021-09-19 19:48:47 -0700

    Run `bump_pyproject.py`

--- tooling/setup.ps1
+++ tooling/setup.ps1
@@ -4,4 +4,5 @@ py -3.9 -m venv .venv
 pip install -U pip  # throws [WinError 5], but still works on its own
 pip install wheel
 pip install -r tooling/requirements_dev.txt
+python tooling/bump_pyproject.py
 flit install -s
